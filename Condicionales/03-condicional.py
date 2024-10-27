c2c87870145ddce8d1c0e5be45fe804b47bf92b7
AndresFelipeTrujillo18 <166618620+AndresFelipeTrujillo18@users.noreply.github.com>
2024-10-27 14:05:32 -0500

    Delete Condicionales directory

--- Condicionales/03-condicional.py
+++
@@ -1,9 +0,0 @@
-#ejercicio 3
-numero = float(input("Digite un numero"))
-
-if numero == 0:
-    print("El numero es cero")
-elif numero < 0: 
-    print("El numero es negativo")
-else:
-    print("El numero es positivo")
--- Condicionales/04-condicional.py
+++
@@ -1,8 +0,0 @@
-#Ejercicio 4
-num1 = float(input("Digite el primer numero "))
-num2 = float(input("Digite el segundo numero "))
-
-if num1 > num2:
-    print(f"{num1} es mayor a {num2}")
-else:
-    print(f"{num2} es mayor a {num1}")
